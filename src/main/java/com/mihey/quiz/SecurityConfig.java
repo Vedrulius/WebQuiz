dce6067aa1ef37befc5f656bd09f7e56205b0f54
Mikhail Dolgov <otetc82@yandex.ru>
2020-10-12 12:04:37 +0300

    final stage 5

--- src/main/java/com/mihey/quiz/SecurityConfig.java
+++ src/main/java/com/mihey/quiz/SecurityConfig.java
@@ -34,10 +34,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().headers().frameOptions().disable();
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
